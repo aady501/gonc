269597891b86c4a444fca7cc44cbed4089e27fcc
aadil <aadil.shaikh1703@gmail.com>
2021-05-01 17:39:11 -0400

    use bufio

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -15,6 +15,7 @@ import (
 	"encoding/base64"
 	"time"
 	randMath "math/rand"
+	"bufio"
 	//"hash"
 )
 
@@ -26,8 +27,8 @@ type Progress struct {
 func tcp_con_handle(params ...net.Conn) {
 	con := params[0]
 	if len(params) == 1 {
-		chan_to_stdout := stream_copy(con, os.Stdout, false)
-		chan_to_remote := stream_copy(os.Stdin, con, true)
+		chan_to_stdout := stream_copy(con, os.Stdout, false, true)
+		chan_to_remote := stream_copy(os.Stdin, con, true, false)
 		select {
 		case <-chan_to_stdout:
 			log.Println("Remote connection is closed")
@@ -36,8 +37,8 @@ func tcp_con_handle(params ...net.Conn) {
 		}
 	}else{
 		proxy_con := params[1]
-		chan_to_stdout := stream_copy(con, proxy_con, false)
-		chan_to_remote := stream_copy(proxy_con, con, true)
+		chan_to_stdout := stream_copy(con, proxy_con, false, false)
+		chan_to_remote := stream_copy(proxy_con, con, true, false)
 		select {
 		case <-chan_to_stdout:
 			log.Println("Remote connection is closed")
@@ -62,8 +63,10 @@ func genSalt() string {
 
 
 // Performs copy operation between streams: os and tcp streams
-func stream_copy(src io.Reader, dst io.Writer,encrypt bool) <-chan int {
-	//buf := make([]byte, 1024)
+func stream_copy(src io.Reader, dst io.Writer,encrypt bool, flush bool) <-chan int {
+	buf_dst := bufio.NewWriter(dst)
+	buf_src := bufio.NewReader(src)
+	buf := make([]byte, 1024)
 	sync_channel := make(chan int)
 	go func() {
 		defer func() {
@@ -74,18 +77,19 @@ func stream_copy(src io.Reader, dst io.Writer,encrypt bool) <-chan int {
 			sync_channel <- 0 // Notify that processing is finished
 		}()
 		for {
-			buf := make([]byte, 1024)
+			//buf := make([]byte, 1024)
 			var nBytes int
 			var err error
-			nBytes, err = src.Read(buf)
+			nBytes, err = buf_src.Read(buf)
 			if err != nil {
 				if err != io.EOF {
 					log.Printf("Read error: %s\n", err)
+				}else{
+					log.Println("Error in Read: %s\n",err)
 				}
-				break
 			}
 			if encrypt{
-				log.Println("In Encryption: ", buf, ", String: ", string(buf[:]))
+				//log.Println("In Encryption: ", buf, ", String: ", string(buf[:]))
 				//salt := bytes.NewbufferString(genSalt()).Bytes()
 				salt := make([]byte, 8)
 				key := pbkdf2.Key([]byte("aadil") , salt, 4096, 32, sha256.New)
@@ -110,12 +114,16 @@ func stream_copy(src io.Reader, dst io.Writer,encrypt bool) <-chan int {
 				//copy(output[:len(nonce)], nonce)
 				//copy(output[len(nonce):], ciphertext)
 
-				_, err = dst.Write(ciphertext)
+				_, err = buf_dst.Write(ciphertext)
 				if err != nil {
 					log.Fatalf("Write error: %s\n", err)
 				}
+					/*err = buf_dst.Flush()
+					if err != nil{
+						log.Fatalf("Flush Error: %s\n", err)
+					}*/
 				//log.Println("Encrypted ",ciphertext, ",", string(ciphertext[:]), "Length of ciphertext: ", len(ciphertext))
-				log.Println("Before Encryption: ",  string(buf[:nBytes]), ", Encrypted: ", string(ciphertext[:]))
+				log.Println("Before Encryption: ", string(buf[:nBytes]),"Encrypted: ", string(ciphertext[:]))
 			}else{
 				salt := make([]byte, 8)
 				key := pbkdf2.Key([]byte("aadil") , salt, 4096, 32, sha256.New)
@@ -140,10 +148,16 @@ func stream_copy(src io.Reader, dst io.Writer,encrypt bool) <-chan int {
 				if err != nil {
 					log.Fatalf(err.Error())
 				}
-				_, err = dst.Write(plaintext)
+				_, err = buf_dst.Write(plaintext)
 				if err != nil {
 					log.Fatalf("Write error: %s\n", err)
 				}
+				/*if flush{
+					err = buf_dst.Flush()
+					if err != nil{
+						log.Fatalf("Flush Error: %s\n", err)
+					}
+				}*/
 				//log.Println("Decrypted: ", plaintext[:], ", ", string(plaintext[:]))
 				log.Println("Before Decryption: " ,string(buf[:nBytes]),"Decrypted: ", string(plaintext[:]))
 			}
