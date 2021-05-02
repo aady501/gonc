02ebdbec4f5bc471c45c9ba3b16cdbdce8954660
aadil <aadil.shaikh1703@gmail.com>
2021-05-02 02:47:13 -0400

    multiplex connections

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -66,18 +66,17 @@ func genSalt() string {
 func stream_copy(src io.Reader, dst io.Writer,encrypt bool, flush bool) <-chan int {
 	buf_dst := bufio.NewWriter(dst)
 	buf_src := bufio.NewReader(src)
-	buf := make([]byte, 1024)
 	sync_channel := make(chan int)
 	go func() {
 		defer func() {
 			if con, ok := dst.(net.Conn); ok {
-				con.Close()
+				//con.Close()
 				log.Printf("Connection from %v is closed\n", con.RemoteAddr())
 			}
 			sync_channel <- 0 // Notify that processing is finished
 		}()
 		for {
-			//buf := make([]byte, 1024)
+			buf := make([]byte, 1024)
 			var nBytes int
 			var err error
 			nBytes, err = buf_src.Read(buf)
@@ -175,60 +174,33 @@ func stream_copy(src io.Reader, dst io.Writer,encrypt bool, flush bool) <-chan i
 }
 
 
-
-// TransferStreams launches two read-write goroutines and waits for signal from them
-func TransferStreams(params ...net.Conn) {
-	con := params[0]
-	c := make(chan Progress)
-
-	// Read from Reader and write to Writer until EOF
-	copy := func(r io.ReadCloser, w io.WriteCloser) {
-		defer func() {
-			r.Close()
-			w.Close()
-		}()
-		//buf:=new(bytes.Buffer)
-		//buf.ReadFrom(r)
-		log.Printf("Hey is someone there")
-		n, err := io.Copy(w, r)
-		if err != nil {
-			log.Printf("[%s]: ERROR: %s\n", con.RemoteAddr(), err)
-		}
-		c <- Progress{bytes: uint64(n)}
-	}
-
-	if len(params) == 1 {
-		go copy(con, os.Stdout)
-		go copy(os.Stdin, con)
-	}else{
-		proxy_con := params[1]
-		go copy(con, proxy_con)
-		go copy(proxy_con, con)
-	}
-	p := <-c
-	log.Printf("[%s]: Connection has been closed by remote peer, %d bytes has been received\n", con.RemoteAddr(), p.bytes)
-	p = <-c
-	log.Printf("[%s]: Local peer has been stopped, %d bytes has been sent\n", con.RemoteAddr(), p.bytes)
-}
-
 // StartServer starts TCP listener
 func StartServer(proto string, port string, listen_port string, host string) {
+	//connect_to_proxy := true
 	ln, err := net.Listen(proto, listen_port)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	log.Println("Listening on", proto+listen_port)
-	con, err := ln.Accept()
-	if err != nil {
-		log.Fatalln(err)
+	//var proxy_con net.Conn
+	for{
+		log.Println("Waiting for new connection")
+		con, err := ln.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("[%s]: Connection has been opened for client\n", con.RemoteAddr())
+
+		//if connect_to_proxy {
+			proxy_con, err := net.Dial(proto, host+port)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Printf("Proxy Connected")
+			//connect_to_proxy = false
+		//
+		go tcp_con_handle(con, proxy_con)
 	}
-	log.Printf("[%s]: Connection has been opened\n", con.RemoteAddr())
-	proxy_con, err := net.Dial(proto, host+port)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("Proxy Connected")
-	tcp_con_handle(con, proxy_con)
 }
 
 // StartClient starts TCP connector
